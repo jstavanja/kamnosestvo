d8014474ea7d5ca8bb50cdffce243308fdebe398
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2018-06-09 15:40:17 +0200

    initial code

---
+++ index.js
@@ -0,0 +1,121 @@
+$(document).ready(() => {
+  $('.navigation').height($(window).height())
+  $('#fullpage').fullpage({
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+    menu: '#menu',
+    slidesNavigation: true,
+    scrollingSpeed: 1000,
+    autoScrolling: true
+  });
+
+  setTimeout(() => {
+    $('.website-container').addClass('navigation-open');
+    $('.page-welcome .card').addClass('fadeInRight');
+  }, 100);
+
+  $('.close-modal').on('click', () => {
+    closeModal('.modal');
+  }) 
+  
+})
+
+// google maps stuff
+function initMap() {
+  var place = {lat: 45.773546, lng: 14.219955};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: place,
+    styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#38414e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#746855'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{color: '#2f3948'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#17263c'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]
+      }
+    ]
+  });
+  var marker = new google.maps.Marker({
+    position: place,
+    map: map
+  });
+}
+
+const openModal = (modalSelector) => {
+  $(modalSelector).addClass('modal-open');
+}
+
+const closeModal = (modalSelector) => {
+  $(modalSelector).removeClass('modal-open');
+}
